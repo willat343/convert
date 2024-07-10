e783aa40fdfa7bd975c5e8d3a075882b8cef5be7
William Talbot <william.talbot97@gmail.com>
2024-07-10 14:59:45 +0200

    added conversion functions for transforms

--- include/convert/eigen/geometry.hpp
+++ include/convert/eigen/geometry.hpp
@@ -10,10 +10,10 @@ namespace convert {
  * @brief Convert a vector representing an angle-axis rotation in form \f$\mathbf{r} = \Vert \mathbf{r} \Vert
  * \mathbf{\hat{r}}\f$ to angle-axis object. If the angle \f$\Vert \mathbf{r} \Vert == 0\f$, then the unit X axis is
  * used.
- * 
- * @tparam Scalar 
- * @param v 
- * @param aa 
+ *
+ * @tparam Scalar
+ * @param v
+ * @param aa
  */
 template<typename Scalar>
 void to(const Eigen::Matrix<Scalar, 3, 1>& v, Eigen::AngleAxis<Scalar>& aa);
@@ -21,13 +21,65 @@ void to(const Eigen::Matrix<Scalar, 3, 1>& v, Eigen::AngleAxis<Scalar>& aa);
 /**
  * @brief Convert an angle-axis object to a vector \f$\mathbf{r}\f$ by multiplying the angle \f$\Vert \mathbf{r}
  * \Vert\f$ by the rotation axis \f$\mathbf{\hat{r}}\f$.
- * 
- * @tparam Scalar 
- * @param aa 
- * @param v 
+ *
+ * @tparam Scalar
+ * @param aa
+ * @param v
  */
 template<typename Scalar>
 void to(const Eigen::AngleAxis<Scalar>& aa, Eigen::Matrix<Scalar, 3, 1>& v);
+
+/**
+ * @brief Convert translation and quaternion to transform.
+ * 
+ * @tparam Scalar 
+ * @tparam D 
+ * @param t 
+ * @param q 
+ * @param T 
+ */
+template<typename Scalar, int D>
+void to(const Eigen::Translation<Scalar, D>& t, const Eigen::Quaternion<Scalar>& q,
+        Eigen::Transform<Scalar, D, Eigen::Isometry>& T);
+
+/**
+ * @brief Convert translation vector and quaternion to transform.
+ * 
+ * @tparam Scalar 
+ * @tparam D 
+ * @param t 
+ * @param q 
+ * @param T 
+ */
+template<typename Scalar, int D>
+void to(const Eigen::Matrix<Scalar, D, 1>& t, const Eigen::Quaternion<Scalar>& q,
+        Eigen::Transform<Scalar, D, Eigen::Isometry>& T);
+
+/**
+ * @brief Convert translation and rotation matrix to transform.
+ * 
+ * @tparam Scalar 
+ * @tparam D 
+ * @param t 
+ * @param R 
+ * @param T 
+ */
+template<typename Scalar, int D>
+void to(const Eigen::Translation<Scalar, D>& t, const Eigen::Matrix<Scalar, D, D>& R,
+        Eigen::Transform<Scalar, D, Eigen::Isometry>& T);
+
+/**
+ * @brief Convert translation vector and rotation matrix to transform.
+ * 
+ * @tparam Scalar 
+ * @tparam D 
+ * @param t 
+ * @param R 
+ * @param T 
+ */
+template<typename Scalar, int D>
+void to(const Eigen::Matrix<Scalar, D, 1>& t, const Eigen::Matrix<Scalar, D, D>& R,
+        Eigen::Transform<Scalar, D, Eigen::Isometry>& T);
 
 }
 
--- include/convert/eigen/impl/geometry.hpp
+++ include/convert/eigen/impl/geometry.hpp
@@ -17,6 +17,30 @@ inline void to(const Eigen::AngleAxis<Scalar>& aa, Eigen::Matrix<Scalar, 3, 1>& 
     v = aa.angle() * aa.axis();
 }
 
+template<typename Scalar, int D>
+inline void to(const Eigen::Translation<Scalar, D>& t, const Eigen::Quaternion<Scalar>& q,
+        Eigen::Transform<Scalar, D, Eigen::Isometry>& T) {
+    T = t * q;
+}
+
+template<typename Scalar, int D>
+inline void to(const Eigen::Matrix<Scalar, D, 1>& t, const Eigen::Quaternion<Scalar>& q,
+        Eigen::Transform<Scalar, D, Eigen::Isometry>& T) {
+    to(Eigen::Translation<Scalar, D>{t}, q, T);
+}
+
+template<typename Scalar, int D>
+void to(const Eigen::Translation<Scalar, D>& t, const Eigen::Matrix<Scalar, D, D>& R,
+        Eigen::Transform<Scalar, D, Eigen::Isometry>& T) {
+    to(t, Eigen::Quaternion<Scalar>{R}, T);
+}
+
+template<typename Scalar, int D>
+inline void to(const Eigen::Matrix<Scalar, D, 1>& t, const Eigen::Matrix<Scalar, D, D>& R,
+        Eigen::Transform<Scalar, D, Eigen::Isometry>& T) {
+    to(Eigen::Translation<Scalar, D>{t}, Eigen::Quaternion<Scalar>{R}, T);
+}
+
 }
 
 #endif
