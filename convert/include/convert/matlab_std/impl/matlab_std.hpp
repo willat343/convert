33d03ef5d158079246b4ed262fdd004196938da5
William Talbot <william.talbot97@gmail.com>
2025-07-08 19:51:54 +0200

    added conversions between matlab struct and chrono time points

--- convert/include/convert/matlab_std/impl/matlab_std.hpp
+++ convert/include/convert/matlab_std/impl/matlab_std.hpp
@@ -23,6 +23,22 @@ void to(const matlab::data::TypedArray<Scalar>& array, std::vector<Scalar>& vect
     std::copy(array.cbegin(), array.cend(), vector.begin());
 }
 
+template<cppbox::IsTimePoint TimePoint>
+void to(const matlab::data::Struct& time_struct, TimePoint& time) {
+    time = TimePoint{std::chrono::seconds(static_cast<int>(time_struct["seconds"][0])) +
+                     std::chrono::nanoseconds(static_cast<int>(time_struct["nanoseconds"][0]))};
+}
+
+template<cppbox::IsTimePoint TimePoint>
+void to(const matlab::data::StructArray& times_struct_array, std::vector<TimePoint>& times) {
+    const std::size_t size = times_struct_array.getNumberOfElements();
+    times.resize(size);
+    for (std::size_t i = 0; i < size; ++i) {
+        const matlab::data::Struct& time_struct = times_struct_array[i];
+        to(time_struct, times[i]);
+    }
+}
+
 template<typename Scalar>
 inline std::enable_if_t<std::is_fundamental_v<Scalar>, matlab::data::TypedArray<Scalar>> to_array(const Scalar& scalar,
         matlab::data::ArrayFactory& factory) {
@@ -44,6 +60,27 @@ matlab::data::CellArray to_cells_of_arrays(const std::vector<T>& vector, matlab:
     std::transform(vector.cbegin(), vector.cend(), cells.begin(),
             [&factory](const T& t) { return to_array(t, factory); });
     return cells;
+}
+
+template<cppbox::IsTimePoint TimePoint>
+matlab::data::StructArray to_struct_array(const TimePoint& time, matlab::data::ArrayFactory& factory) {
+    matlab::data::StructArray time_array = factory.createStructArray({1, 1}, {{"seconds"}, {"nanoseconds"}});
+    time_array[0]["seconds"] = convert::to_array(time.time_since_epoch() / std::chrono::seconds(1), factory);
+    time_array[0]["nanoseconds"] =
+            convert::to_array((time.time_since_epoch() % std::chrono::seconds(1)).count(), factory);
+    return time_array;
+}
+
+template<cppbox::IsTimePoint TimePoint>
+matlab::data::StructArray to_struct_array(const std::vector<TimePoint>& times, matlab::data::ArrayFactory& factory) {
+    matlab::data::StructArray times_array =
+            factory.createStructArray({1, times.size()}, {{"seconds"}, {"nanoseconds"}});
+    for (std::size_t i = 0; i < times.size(); ++i) {
+        times_array[i]["seconds"] = convert::to_array(times[i].time_since_epoch() / std::chrono::seconds(1), factory);
+        times_array[i]["nanoseconds"] =
+                convert::to_array((times[i].time_since_epoch() % std::chrono::seconds(1)).count(), factory);
+    }
+    return times_array;
 }
 
 }
--- convert/include/convert/matlab_std/matlab_std.hpp
+++ convert/include/convert/matlab_std/matlab_std.hpp
@@ -1,6 +1,7 @@
 #ifndef CONVERT_MATLAB_STD_MATLAB_STD_HPP
 #define CONVERT_MATLAB_STD_MATLAB_STD_HPP
 
+#include <cppbox/time.hpp>
 #include <mex.hpp>
 #include <type_traits>
 #include <vector>
@@ -13,6 +14,12 @@ void to(const matlab::data::TypedArray<Scalar>& array, Scalar& scalar);
 template<typename Scalar>
 void to(const matlab::data::TypedArray<Scalar>& array, std::vector<Scalar>& vector);
 
+template<cppbox::IsTimePoint TimePoint>
+void to(const matlab::data::Struct& time_struct, TimePoint& time);
+
+template<cppbox::IsTimePoint TimePoint>
+void to(const matlab::data::StructArray& times_struct_array, std::vector<TimePoint>& times);
+
 template<typename Scalar>
 std::enable_if_t<std::is_fundamental_v<Scalar>, matlab::data::TypedArray<Scalar>> to_array(const Scalar& scalar,
         matlab::data::ArrayFactory& factory);
@@ -22,6 +29,12 @@ matlab::data::TypedArray<Scalar> to_array(const std::vector<Scalar>& vector, mat
 
 template<typename T>
 matlab::data::CellArray to_cells_of_arrays(const std::vector<T>& vector, matlab::data::ArrayFactory& factory);
+
+template<cppbox::IsTimePoint TimePoint>
+matlab::data::StructArray to_struct_array(const TimePoint& time, matlab::data::ArrayFactory& factory);
+
+template<cppbox::IsTimePoint TimePoint>
+matlab::data::StructArray to_struct_array(const std::vector<TimePoint>& times, matlab::data::ArrayFactory& factory);
 
 }
 
