e908b3f580d0ba6b0f8dad67cd065e36d65cab4b
William Talbot <william.talbot97@gmail.com>
2025-07-26 19:55:04 +0200

    removed child_frame from IMU

--- src/sensorbox_ros/imu.cpp
+++ src/sensorbox_ros/imu.cpp
@@ -7,7 +7,6 @@ namespace convert {
 
 void to(const sensor_msgs::Imu& msg, sensorbox::ImuMeasurement<3>& imu) {
     to(msg.header, imu.timestamp(), imu.frame());
-    imu.child_frame() = imu.frame();
     to(msg.angular_velocity, imu.angular_velocity());
     to(msg.linear_acceleration, imu.linear_acceleration());
 }
