ac2bdcac282ed362498ca99f5e3a0952eb9def82
William Talbot <william.talbot97@gmail.com>
2025-07-28 17:38:16 +0200

    removed sensorbox from convert (moved to sensorbox)

--- include/convert/sensorbox_ros/sensorbox_ros.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CONVERT_SENSORBOX_ROS_SENSORBOX_ROS_HPP
-#define CONVERT_SENSORBOX_ROS_SENSORBOX_ROS_HPP
-
-#include "convert/sensorbox_ros/imu.hpp"
-#include "convert/sensorbox_ros/pose.hpp"
-#include "convert/sensorbox_ros/pose_twist.hpp"
-
-#endif
