8936ec08e957de48c4cfb3bcd74f883a0e20566b
William Talbot <william.talbot97@gmail.com>
2026-08-04 16:21:17 +0200

    clang formatting rearranging headers

--- test/src/manif_json/manif_json.cpp
+++ test/src/manif_json/manif_json.cpp
@@ -1,6 +1,6 @@
-#include <gtest/gtest.h>
-
 #include "convert/manif_json/manif_json.hpp"
+
+#include <gtest/gtest.h>
 
 TEST(json_r1, to_from_d) {
     const manif::R1d in(Eigen::Matrix<double, 1, 1>(1.0));
